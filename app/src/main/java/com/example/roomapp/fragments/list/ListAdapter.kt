d27927710aeb970da0a8e6e1e815e711c679aaef
Nur Annisa Yusrah Putra Djaya <nurannisayusrahpdj@gmail.com>
2021-06-11 22:26:18 +0800

    Aplikasi List Barang Elektronik

---
+++ app/src/main/java/com/example/roomapp/fragments/list/ListAdapter.kt
@@ -0,0 +1,42 @@
+package com.example.roomapp.fragments.list
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.findNavController
+import androidx.recyclerview.widget.RecyclerView
+import com.example.roomapp.R
+import com.example.roomapp.model.User
+import kotlinx.android.synthetic.main.custom_row.view.*
+
+class ListAdapter: RecyclerView.Adapter<ListAdapter.MyViewHolder>() {
+
+    private var userList = emptyList<User>()
+
+    class MyViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {}
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        return MyViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.custom_row, parent, false))
+    }
+
+    override fun getItemCount(): Int {
+       return userList.size
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        val currentItem = userList[position]
+        holder.itemView.firstName_txt.text = currentItem.NamaBarang
+        holder.itemView.lastName_txt.text = currentItem.HargaBarang
+        holder.itemView.age_txt.text = currentItem.Kuantitas.toString()
+
+        holder.itemView.rowLayout.setOnClickListener {
+            val action = ListFragmentDirections.actionListFragmentToUpdateFragment(currentItem)
+            holder.itemView.findNavController().navigate(action)
+        }
+    }
+
+    fun setData(user: List<User>){
+        this.userList = user
+        notifyDataSetChanged()
+    }
+}
